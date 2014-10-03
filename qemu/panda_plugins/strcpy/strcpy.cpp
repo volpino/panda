47b1381a92a32122b33b87305faf4faed3b15cb1
Patrick Hulin <patrick.hulin@gmail.com>
2014-10-03 15:09:58 -0400

    Convert strcpy plugin to C++ and send output to file.

--- qemu/panda_plugins/strcpy/strcpy.cpp
+++ qemu/panda_plugins/strcpy/strcpy.cpp
@@ -15,6 +15,8 @@ PANDAENDCOMMENT */
 // the PRIx64 macro
 #define __STDC_FORMAT_MACROS
 
+extern "C" {
+
 #include "config.h"
 #include "qemu-common.h"
 #include "rr_log.h"
@@ -29,6 +31,16 @@ void uninit_plugin(void *);
 
 int before_block_exec(CPUState *env, TranslationBlock *tb);
 
+} // extern "C"
+
+#include <set>
+#include <vector>
+#include <string>
+
+static std::set<target_ulong> tainted_strcpys;
+
+static FILE *strcpy_log;
+
 uint32_t get_stack(CPUState *env, int offset) {
     uint32_t result = 0;
     panda_virtual_memory_rw(env, env->regs[R_ESP] + 4 * offset, (uint8_t *)&result, 4, 0);
@@ -41,11 +53,15 @@ int before_block_exec(CPUState *env, TranslationBlock *tb) {
         // right process.
         if (tb->pc == 0x080646c0) {
             target_ulong src = get_stack(env, 2);
-            printf("strcpy: ret=%lx, str=%lx\n", get_stack(env, 0), src);
-            char buf[4096];
-            panda_virtual_memory_rw(env, get_stack(env, 2), (uint8_t *)buf, 4096, 0);
-            //printf("    %s\n", buf);
-            printf("     %lu\n", taint_query_ram(panda_virt_to_phys(env, src)));
+            uint32_t num_labels = taint_query_ram(panda_virt_to_phys(env, src));
+            if (num_labels > 0) {
+                target_ulong ret = get_stack(env, 0);
+                fprintf(strcpy_log, "TAINTED strcpy: ret=%lx, str=%lx, num=%u\n", ret, src, num_labels);
+                char buf[200];
+                panda_virtual_memory_rw(env, get_stack(env, 2), (uint8_t *)buf, 200, 0);
+                fprintf(strcpy_log, "    %s\n", buf);
+                tainted_strcpys.insert(ret);
+            }
         }
     }
 
@@ -56,11 +72,16 @@ bool init_plugin(void *self) {
     panda_cb pcb = { .before_block_exec = before_block_exec };
     panda_register_callback(self, PANDA_CB_BEFORE_BLOCK_EXEC, pcb);
 
+    strcpy_log = fopen("strcpy.txt", "w");
+
     init_taint_api();
 
     return true;
 }
 
 void uninit_plugin(void *self) {
-
+    for (auto it = tainted_strcpys.begin(); it != tainted_strcpys.end(); it++) {
+        printf("0x%lx\n", *it);
+    }
+    printf("\n");
 }
